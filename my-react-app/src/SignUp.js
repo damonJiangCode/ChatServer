209e9c38ad9b559c89638991fd8263b04b1eba3c
Damon Jiang <damonjiangwork@gmail.com>
2023-11-17 23:28:45 -0600

    basic logic done, wait for db

--- my-react-app/src/SignUp.js
+++ my-react-app/src/SignUp.js
@@ -1,68 +1,89 @@
 import './SignUp.css';
+import React, { useState } from 'react';
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    passwordCheck:'',
+    email: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.passwordCheck) {
+      alert('Passwords do not match!');
+      setFormData({
+        ...formData,
+        password: '',
+        passwordCheck: '',
+      });
+    } else {
+      // insert into database
+    }
+  };
+
   return (
-    <div>
-      <p>
-        Sign Up Page
-      </p>
+    <div className='container'>
+      <form className='form' onSubmit={handleSubmit}>
+
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            name="passwordCheck"
+            value={formData.passwordCheck}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+
+        <button className='button' type="submit">Submit</button>
+
+      </form>
     </div>
   );
 }
-// const LogIn = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleUsernameChange = (e) => {
-//     setUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Add your login logic here (e.g., send data to a server, validate credentials)
-//     console.log('Username:', username);
-//     console.log('Password:', password);
-//   };
-
-//   return (
-//     <div>
-//         <form className="login-form" onSubmit={handleSubmit}>
-
-//             <div className="form-group">
-//                 <label>
-//                 Username:
-//                 <input
-//                     type="text"
-//                     value={username}
-//                     onChange={handleUsernameChange}
-//                     required
-//                 />
-//                 </label>
-//             </div>
-
-//             <div className="form-group">
-//                 <label>
-//                 Password:
-//                 <input
-//                     type="password"
-//                     value={password}
-//                     onChange={handlePasswordChange}
-//                     required
-//                 />
-//                 </label>
-//             </div>
-
-//             <button type="submit" className="submit-button">Log In</button>
-
-//             <button type="submit" className="submit-button">Sign Up</button>
-
-//         </form>
-//     </div>
-//   );
-// };
 
 export default SignUp;
