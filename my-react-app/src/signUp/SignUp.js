2ff1de948f23a3465efea7eb5c2dff756bd0dd2a
Damon Jiang <damonjiangwork@gmail.com>
2023-12-02 18:09:10 -0600

    signup completed

--- my-react-app/src/signUp/SignUp.js
+++ my-react-app/src/signUp/SignUp.js
@@ -1,12 +1,15 @@
-import './SignUp.css';
-import React, { useState } from 'react';
+import "./SignUp.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    passwordCheck:'',
-    email: ''
+    username: "",
+    password: "",
+    passwordCheck: "",
+    email: "",
   });
 
   const handleInputChange = (e) => {
@@ -17,25 +20,52 @@ function SignUp() {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (formData.password !== formData.passwordCheck) {
-      alert('Passwords do not match!');
+      alert("Passwords do not match!");
       setFormData({
         ...formData,
-        password: '',
-        passwordCheck: '',
+        password: "",
+        passwordCheck: "",
       });
     } else {
-      // insert into database
+      try {
+        // Make a POST request to the server
+        const response = await fetch("http://localhost:3001/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          console.log("Data successfully submitted");
+          // Optionally, you can redirect the user or perform other actions
+          alert(`Sign Up Successfully`);
+          navigate("/chat", {
+            state: { username: formData.username, email: formData.email },
+          });
+        } else {
+          // Check if the response status is 400 (Bad Request)
+          if (response.status === 400) {
+            const errorData = await response.json();
+            alert(`Error: ${errorData.error}`);
+          } else {
+            console.error("Failed to submit data");
+          }
+        }
+      } catch (error) {
+        console.error("Error during data submission:", error);
+      }
     }
   };
 
   return (
-    <div className='container'>
-      <label className='signUpText'>Sign Up</label>
-      <form className='form' onSubmit={handleSubmit}>
-
+    <div className="container">
+      <label className="signUpText">Sign Up</label>
+      <form className="form" onSubmit={handleSubmit}>
         <label>
           Username:
           <input
@@ -80,8 +110,9 @@ function SignUp() {
         </label>
         <br />
 
-        <button className='button' type="submit">Submit</button>
-
+        <button className="button" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
